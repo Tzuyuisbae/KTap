f6b6c023bfe14da314d2b505987dcc4dcc42046f
Joe Liu <liujoe99@hotmail.ca>
2018-08-24 00:54:10 -0400

    created new package to contain each song's note files

--- KTap/src/KTap.java
+++ KTap/src/KTap.java
@@ -1,9 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 public class KTap {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// create an action listener
+		ActionListener action = new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		
+		// read the notes
+		
+		// start the game
+		new Timer(1000, action).start();
 	}
 
 }
