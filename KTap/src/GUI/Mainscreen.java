eadb634779408c963bed8d0b831f34ea36e06151
Joe Liu <liujoe99@hotmail.ca>
2018-08-31 01:22:35 -0400

    added extra constructor for setting up window based on screen size

--- KTap/src/GUI/Mainscreen.java
+++ KTap/src/GUI/Mainscreen.java
@@ -1,11 +1,26 @@
 package GUI;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Mainscreen extends JFrame{
 
+	// get the screen size
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	
+	// constructor with input sizes
 	public Mainscreen(int width, int height) {
 		this.setSize(width, height);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+	
+	// default constructor
+	public Mainscreen() {
+		this.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
