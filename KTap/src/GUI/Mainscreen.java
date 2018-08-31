3cf46470ab170e5be82ddf197505547b92259e8f
Joe Liu <liujoe99@hotmail.ca>
2018-08-30 23:56:33 -0400

    created mainscreen object and set default values

--- KTap/src/GUI/Mainscreen.java
+++ KTap/src/GUI/Mainscreen.java
@@ -4,7 +4,10 @@ import javax.swing.JFrame;
 
 public class Mainscreen extends JFrame{
 
-	public Mainscreen() {
-		
+	public Mainscreen(int width, int height) {
+		this.setSize(width, height);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
--- KTap/src/KTap.java
+++ KTap/src/KTap.java
@@ -2,10 +2,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
+import GUI.Mainscreen;
+
 public class KTap {
 	
 	public static void main(String[] args) {
 
+		// create the main screen
+		Mainscreen mainScreen = new Mainscreen(1700, 1000);
+		
 		// create an action listener
 		ActionListener action = new ActionListener(){
 
