0ed7093f43994cb71a843559293b703427e10d45
Joe Liu <liujoe99@hotmail.ca>
2018-09-25 14:34:25 -0400

    added a delay variable and keyboard input check

--- KTap/src/KTap.java
+++ KTap/src/KTap.java
@@ -1,5 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.Timer;
 
 import GUI.Mainscreen;
@@ -11,21 +13,40 @@ public class KTap {
 		// create the main screen
 		Mainscreen mainScreen = new Mainscreen(1700, 1000);
 		
+		// set the delay
+		int delay = 100;
+		
 		// create an action listener
 		ActionListener action = new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+				System.out.println("test");
 			}
+			
+		    public void keyPressed(KeyEvent e) {
+		        System.out.println("KEY PRESSED: ");
+		    }
 			
 		};
 		
 		// read the notes
 		
 		// start the game
-		new Timer(1000, action).start();
+		new Timer(delay, action).start();
+		
+//		Timer timer = new Timer(40, new ActionListener() {
+//		    @Override
+//		    public void actionPerformed(ActionEvent ae) {
+//
+//		    }
+//		});
+		
+	}
+	
+	public void moveNote() {
+		
 	}
 
 }
